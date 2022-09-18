3df7541af1bbb044bfaf6206759c64222e899d9c
Hbyrum2003 <113479844+Hbyrum2003@users.noreply.github.com>
2022-09-18 13:27:03 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,18 @@
+
+
+public class SalesTax {
+
+    public static void main(String[] args) {
+       double salesTax = 0.05;
+       double productPrice = 10;
+       double totalCost;
+       totalCost = productPrice*salesTax;
+       totalCost = totalCost + productPrice;
+        System.out.println("The total cost is:" + totalCost);
+
+
+
+
+
+    }
+}
